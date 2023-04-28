2e5c64ec9b7531cd6dcbaef1b8d364e9d00e1727
Aleksandr Sukonko <aleksandr.sukonko@gmail.com>
2023-04-28 14:02:37 +0300

    version 1

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,11 @@
+﻿//for(int i = 0; i < 10; i++)
+//for(int i = 10; i > 0; i--)    //i-- = i - 1
+int sum = 0;
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i;  //sum += i;
+}
+
+Console.WriteLine($"Final total {sum}");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; //bool - true/false
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4); // (1, 4) kuni 3ni
+
+    //Console.WriteLine($"cpu has generated {cpuRandom}."); //cheat
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+
+    int userNumber = Int32.Parse(Console.ReadLine());   //konverteerime string numbriks
+
+    if(userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        //loopActive = false;
+        break;  //lõpeb tsükli while loopi, sama mis loopActive = false;
+    }
+    else 
+    {
+        Console.WriteLine("Oops. Try again!");
+    }
+}
+
+Console.WriteLine("Have a nice day!");
